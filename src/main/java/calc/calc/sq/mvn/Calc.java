39c110299303b3bb3344c8824119480315f6062e
kumar.lakshmanappa <kumar.lakshmanappa@BDC8-LX-3S0KPQ2.dir.svc.accenture.com>
2019-09-06 10:55:14 +0530

    Code updated

--- src/main/java/calc/calc/sq/mvn/Calc.java
+++ src/main/java/calc/calc/sq/mvn/Calc.java
@@ -4,26 +4,21 @@ package calc.calc.sq.mvn;
  */
 public class Calc extends java.lang.Thread{
     public double doAdd(double a, double b){
-    	double sum = Double.longBitsToDouble(123);
-    	sum = a + b;
-        return sum;
+     return a + b;
     }
     public double doSub(double num1, double num2){
-    	int value = new Integer(123);
-        double SUB = 0;
-        SUB = num1 - num2;
-    	return SUB;
+     return num1 - num2;      
     }
     public double doMul(double num1, double num2){
         return num1 * num2;
     }
     public double doDiv(double num1, double num2) throws Exception{
         if(num2 == 0){
-        	throw new Exception();
+         throw new Exception();
         }
-    	return num1 / num2;
+     return num1 / num2;
     }
-	public double changeSign(double num){
+public double changeSign(double num){
         return -1 * num;
     }
 }
